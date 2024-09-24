96aa66f6c3887ae1423092ab6b892aff19d7c1a7
Furhan Ahmed <itz.farhan6710@gmail.com>
2024-09-24 22:02:31 +0530

    error-solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import AboutUs from "@/components/AboutUs";
 import Faq from "@/components/Faq";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-      
       <AboutUs />
       <Faq />
-      <Footer />
     </>
   );
 }
